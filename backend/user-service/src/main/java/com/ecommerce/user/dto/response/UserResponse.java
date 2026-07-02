70e3b9d7c948dc134cb44272fbf8b21fdfb5ad4f
devang thummar <devangthummar877@gmail.com>
2026-07-02 17:53:01 +0530

    feat(user): add request and response DTOs

---
+++ backend/user-service/src/main/java/com/ecommerce/user/dto/response/UserResponse.java
@@ -0,0 +1,22 @@
+package com.ecommerce.user.dto.response;
+
+import com.ecommerce.user.entity.enums.Role;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserResponse {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private Role role;
+}
