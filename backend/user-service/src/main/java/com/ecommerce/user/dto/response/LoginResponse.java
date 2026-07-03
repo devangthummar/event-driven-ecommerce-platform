f9d530a78ac75760e7ac8a17d12c7937dc15227b
devang thummar <devangthummar877@gmail.com>
2026-07-03 16:56:39 +0530

    feat(user): implement custom user details for authentication

---
+++ backend/user-service/src/main/java/com/ecommerce/user/dto/response/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.ecommerce.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String accessToken;
+
+    private String tokenType;
+
+}
---
+++ backend/user-service/src/main/java/com/ecommerce/user/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.ecommerce.user.security;
+
+import com.ecommerce.user.entity.User;
+import com.ecommerce.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
+
+}
