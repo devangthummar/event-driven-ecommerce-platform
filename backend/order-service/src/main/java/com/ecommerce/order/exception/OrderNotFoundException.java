366655f2bc1bc505bf8b67057b0d44a589f5d363
devang thummar <devangthummar877@gmail.com>
2026-07-11 18:58:16 +0530

    feat(order): implement global exception handling

---
+++ backend/order-service/src/main/java/com/ecommerce/order/exception/OrderNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ecommerce.order.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+}
