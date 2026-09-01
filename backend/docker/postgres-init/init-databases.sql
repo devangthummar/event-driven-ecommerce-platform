88f33e2fae4aba397df33a5ca7a325efb4ef058f
devang thummar <devangthummar877@gmail.com>
2026-09-01 12:17:02 +0530

    feat(docker): containerize all services with multi-stage builds and docker-compose

---
+++ backend/docker/postgres-init/init-databases.sql
@@ -0,0 +1,6 @@
+-- Create all microservice databases
+CREATE DATABASE user_service_db;
+CREATE DATABASE product_service_db;
+CREATE DATABASE order_service_db;
+CREATE DATABASE inventory_db;
+CREATE DATABASE payment_db;
